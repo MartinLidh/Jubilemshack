8f33d1a45c56a2f1b0a7754c8fc8a56544f0718e
Ante Wall <ante@significantbit.se>
2016-02-12 22:06:47 +0100

    added text and text animation to paper

--- js/game.js
+++ js/game.js
@@ -6,8 +6,7 @@ var gameState = {
     //game.add.image(0, 0, 'bg');
     game.add.image(10, 10, 'char');
 
-
-    game.paper = new Paper(game, 100,100, 1);
+    game.paper = new Paper(game, game.width/2, 10, 1);
     //game.add.plugin(Phaser.Plugin.Debug);
     // game.add.plugin(Phaser.Plugin.Inspector);
     game.add.image(0, 0, 'bg');
