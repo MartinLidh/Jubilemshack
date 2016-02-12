93415406e34dd0c8030fdd56c7103b3f0cde0bc8
marli994 <marli994@student.liu.se>
2016-02-12 22:30:54 +0100

    fixed movement

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 var player;
 var cursors;
+var charScale = 0.4;
 
 var gameState = {
 
@@ -14,12 +15,14 @@ var gameState = {
 
       var w = game.width;
       var h = game.height;
+
       game.paper = new Paper(game, this.game.width/2, 0, 1);
     //game.add.plugin(Phaser.Plugin.Debug);
     //game.add.plugin(Phaser.Plugin.Inspector);
     //game.add.image(0, 0, 'bg');
-      player = game.add.sprite(10,10, 'char');
-      player.scale.setTo(0.4,0.4);
+      player = game.add.sprite(w/4,h-100, 'char');
+      player.scale.setTo(charScale,charScale);
+      player.anchor.setTo(0.5,0.5);
       game.physics.enable(player, Phaser.Physics.ARCADE);
       cursors = game.input.keyboard.createCursorKeys();
       game.paper.create();
@@ -33,12 +36,16 @@ var gameState = {
 
     if(cursors.left.isDown)
     {
-      player.body.velocity.x = -400;
+	
+	if(!(player.x < (player.width/2)*charScale)){
+	    player.body.velocity.x = -400;
+	}
     }
 
     else if(cursors.right.isDown)
     {
-      player.body.velocity.x = 400;
+	if(!(player.x > w/2-(player.width/2)*charScale))
+	    player.body.velocity.x = 400;
     }
   }
 
