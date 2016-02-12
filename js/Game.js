a2f3a81bf04a0656b1c36a27efe12ffcc6d07822
Tim Lestander <timle211@stuent.liu.se>
2016-02-12 20:36:23 +0100

    To Martin

--- js/Game.js
+++ js/Game.js
@@ -1,76 +1,69 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+//var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
-function preload() {
+GetAJob = {
 
-    game.load.image('bullet', 'assets/games/invaders/bullet.png');
-    game.load.image('enemyBullet', 'assets/games/invaders/enemy-bullet.png');
+};
+
+GetAJob.Game = function(game) {
+
+};
+
+GetAJob.Game.prototype = {
+
+    preload: function() {
+
+    this.game.load.image('character', 'assets/guy.png');
+   /* game.load.image('enemyBullet', 'assets/games/invaders/enemy-bullet.png');
     game.load.spritesheet('invader', 'assets/games/invaders/invader32x32x4.png', 32, 32);
     game.load.image('ship', 'assets/games/invaders/player.png');
     game.load.spritesheet('kaboom', 'assets/games/invaders/explode.png', 128, 128);
     game.load.image('starfield', 'assets/games/invaders/starfield.png');
-    game.load.image('background', 'assets/games/starstruck/background2.png');
+    game.load.image('background', 'assets/games/starstruck/background2.png'); */
+    },
 
-}
+    create: function() {
 
-var player;
-var aliens;
-var bullets;
-var bulletTime = 0;
-var cursors;
-var fireButton;
-var explosions;
-var starfield;
-var score = 0;
-var scoreString = '';
-var scoreText;
-var lives;
-var enemyBullet;
-var firingTimer = 0;
-var stateText;
-var livingEnemies = [];
+    //game.physics.startSystem(Phaser.Physics.ARCADE);
 
-function create() {
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
+    this.game.add.sprite(0, 0, 'character')
     //  The scrolling starfield background
-    starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+    //starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
 
     //  Our bullet group
-    bullets = game.add.group();
+    /*bullets = game.add.group();
     bullets.enableBody = true;
     bullets.physicsBodyType = Phaser.Physics.ARCADE;
     bullets.createMultiple(30, 'bullet');
     bullets.setAll('anchor.x', 0.5);
     bullets.setAll('anchor.y', 1);
     bullets.setAll('outOfBoundsKill', true);
-    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('checkWorldBounds', true); */
 
     // The enemy's bullets
-    enemyBullets = game.add.group();
+    /* enemyBullets = game.add.group();
     enemyBullets.enableBody = true;
     enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
     enemyBullets.createMultiple(30, 'enemyBullet');
     enemyBullets.setAll('anchor.x', 0.5);
     enemyBullets.setAll('anchor.y', 1);
     enemyBullets.setAll('outOfBoundsKill', true);
-    enemyBullets.setAll('checkWorldBounds', true);
+    enemyBullets.setAll('checkWorldBounds', true); */
 
     //  The hero!
-    player = game.add.sprite(400, 500, 'ship');
+    player = game.add.sprite(400, 500, 'character');
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     //  The baddies!
-    aliens = game.add.group();
+   /* aliens = game.add.group();
     aliens.enableBody = true;
-    aliens.physicsBodyType = Phaser.Physics.ARCADE;
+    aliens.physicsBodyType = Phaser.Physics.ARCADE; */
 
-    createAliens();
+    // createAliens();
 
     //  The score
-    scoreString = 'Score : ';
-    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
+    /*scoreString = 'Score : ';
+    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' }); 
 
     //  Lives
     lives = game.add.group();
@@ -96,11 +89,11 @@ function create() {
 
     //  And some controls to play the game with
     cursors = game.input.keyboard.createCursorKeys();
-    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR); */
     
-}
+},
 
-function createAliens () {
+/* function createAliens () {
 
     for (var y = 0; y < 4; y++)
     {
@@ -122,9 +115,9 @@ function createAliens () {
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
-}
+} */
 
-function setupInvader (invader) {
+/* function setupInvader (invader) {
 
     invader.anchor.x = 0.5;
     invader.anchor.y = 0.5;
@@ -136,12 +129,12 @@ function descend() {
 
     aliens.y += 10;
 
-}
+} */
 
-function update() {
+    update: function() {
 
     //  Scroll the background
-    starfield.tilePosition.y += 2;
+    // starfield.tilePosition.y += 2;
 
     if (player.alive)
     {
@@ -158,33 +151,24 @@ function update() {
         }
 
         //  Firing?
-        if (fireButton.isDown)
+        /* if (fireButton.isDown)
         {
             fireBullet();
-        }
+        } 
 
         if (game.time.now > firingTimer)
         {
             enemyFires();
-        }
+        } */
 
         //  Run collision
-        game.physics.arcade.overlap(bullets, aliens, collisionHandler, null, this);
-        game.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
+        // game.physics.arcade.overlap(bullets, aliens, collisionHandler, null, this);
+        // game.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
     }
 
 }
 
-function render() {
-
-    // for (var i = 0; i < aliens.length; i++)
-    // {
-    //     game.debug.body(aliens.children[i]);
-    // }
-
-}
-
-function collisionHandler (bullet, alien) {
+/* function collisionHandler (bullet, alien) {
 
     //  When a bullet hits an alien we kill them both
     bullet.kill();
@@ -299,9 +283,9 @@ function resetBullet (bullet) {
     //  Called if the bullet goes out of the screen
     bullet.kill();
 
-}
+} */
 
-function restart () {
+/* function restart () {
 
     //  A new level starts
     
@@ -316,4 +300,6 @@ function restart () {
     //hides the text
     stateText.visible = false;
 
-}
+} */
+};
+ 
