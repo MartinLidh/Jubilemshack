c82e9e9361d47e813750e00f82bf2b8659759fff
marli994 <marli994@student.liu.se>
2016-02-12 21:17:06 +0100

    changed some for babb

--- js/game.js
+++ js/game.js
@@ -3,8 +3,8 @@ var gameState = {
     create: function(){
 	//game.add.plugin(Phaser.Plugin.Debug);
 	// game.add.plugin(Phaser.Plugin.Inspector);
-        game.add.image(0, 0, 'bg');
-	game.add.image(150, 800, 'char');
+        //game.add.image(0, 0, 'bg');
+	game.add.image(10, 10, 'char');
       
 
     },
--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ var menuState = {
     create: function(){
 
         //game.add.plugin(Phaser.Plugin.Debug);
-        game.add.image(0, 0, 'bg');
+        //game.add.image(0, 0, 'bg');
 	game.add.image(150, 800, 'char');
        // game.add.plugin(Phaser.Plugin.Inspector);
 	game.state.start('game');
